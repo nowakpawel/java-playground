48287a2ef805f8d46fcf2797fafd27c0e4e74e4c
nowakpawel <kontakt.nowakpawel@gmail.com>
2025-02-09 22:40:11 +0100

    Selection Sort

--- src/main/java/pl/pawel/demo/algotithms/Algorithms.java
+++ src/main/java/pl/pawel/demo/algotithms/Algorithms.java
@@ -1,5 +1,9 @@
 package pl.pawel.demo.algotithms;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Algorithms {
     public int binarySearch(int[] arr, int itemToFind) {
         int low = 0;
@@ -24,5 +28,34 @@ public class Algorithms {
         }
 
         return -1;
+    }
+
+    public List<Integer> selectionSort(List<Integer> unsorted) {
+        int size = unsorted.size();
+
+        List<Integer> newArray = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            Integer smallest = findSmallestElement(unsorted);
+            newArray.add(unsorted.get(smallest));
+            unsorted.remove((int) smallest);
+        }
+
+            return newArray;
+    }
+
+    private int findSmallestElement(List<Integer> arr) {
+        int smallest = arr.get(0);
+        int smallestIndex = 0;
+        int size = arr.size();
+
+        for (int i = 0; i < size; i++) {
+            if (arr.get(i) < smallest) {
+                smallest = arr.get(i);
+                smallestIndex = i;
+            }
+        }
+
+        return smallestIndex;
     }
 }
--- src/main/java/pl/pawel/demo/algotithms/Main.java
+++ src/main/java/pl/pawel/demo/algotithms/Main.java
@@ -1,6 +1,9 @@
 package pl.pawel.demo.algotithms;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     private static Algorithms algorithms = new Algorithms();
     public static void main(String[] args) {
@@ -8,6 +11,13 @@ public class Main {
 
         int[] orderedArray = {-5, -3, 0, 1, 2, 5, 8, 9, 11};
         System.out.println(algorithms.binarySearch(orderedArray, -3));
+
+        printDelimiter("Selection Sort");
+        ArrayList<Integer> unsorted = new ArrayList<>(Arrays.asList(1, -2, 5, 7, 16, -4, 0, 19, 9, 10, 21));
+
+        System.out.println("Unsorted array:\t" + unsorted);
+        System.out.println("SortedList:\t" + algorithms.selectionSort(unsorted));
+
 
     }
 
