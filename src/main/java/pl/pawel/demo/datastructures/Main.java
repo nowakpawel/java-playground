fa43ddf169ae717e47cb9376bb10e3900a423576
nowakpawel <kontakt.nowakpawel@gmail.com>
2025-02-12 14:15:53 +0100

    HashTable init

--- src/main/java/pl/pawel/demo/datastructures/Main.java
+++ src/main/java/pl/pawel/demo/datastructures/Main.java
@@ -2,15 +2,35 @@ package pl.pawel.demo.datastructures;
 
 
 import java.util.HashMap;
+import java.util.Hashtable;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\n===== HashMap ====\n");
+        System.out.println("===== HashTable ====");
+        Hashtable<Integer, String> table = new Hashtable<>(10);
 
-        HashMap<String, String> map = new HashMap<String, String>();
-        map.put("bobyjoe5432", "Fluff!");
-        map.put("helloKity", "Password1234!");
-        map.put("stefan", "12345!");
+        table.put(100, "Spongebob");
+        table.put(123, "Patric");
+        table.put(321, "Sandy");
+        table.put(555, "Squidward");
+        table.put(777, "Gary");
+
+        for (Integer key: table.keySet()) {
+            System.out.println(key.hashCode() % 10 + "\t" + key + "\t" + table.get(key));
+        }
+
+        System.out.println("\n----- String as a key ------\n");
+
+        Hashtable<String, String> stringTable = new Hashtable<>(10);
+
+        stringTable.put("100", "Spongebob");
+        stringTable.put("123", "Patric");
+        stringTable.put("321", "Sandy");
+        stringTable.put("555", "Squidward");
+        stringTable.put("777", "Gary");
+
+        for (String key: stringTable.keySet()) {
+            System.out.println(key.hashCode() % 10 + "\t" + key + "\t" + stringTable.get(key));
+        }
     }
-
 }
